ba155afb7bcc3f5ca07234a7c8a455cbfbf1df2c
二货机器人 <smith3816@gmail.com>
2024-08-19 21:49:55 +0800

    chore: support ts def

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'father';
 export default defineConfig({
   plugins: ['@rc-component/father-plugin'],
   esm: {
-    input: 'components/index.ts',
+    input: 'components/',
+    ignores: ['**/demo/**', '**/__tests__/**'],
+  },
+  cjs: {
+    input: 'components/',
+    ignores: ['**/demo/**', '**/__tests__/**'],
   },
 });
