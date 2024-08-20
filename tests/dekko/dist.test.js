bd67c0506d04c4b8035f1cee1375e79b0480ae9e
二货机器人 <smith3816@gmail.com>
2024-08-20 11:12:45 +0800

    ci: add dekko test

--- tests/dekko/dist.test.js
+++ tests/dekko/dist.test.js
@@ -3,15 +3,14 @@ const chalk = require('chalk');
 
 $('dist')
   .isDirectory()
-  .hasFile('antd-with-locales.js')
-  .hasFile('antd-with-locales.js.map')
-  .hasFile('antd-with-locales.min.js')
-  .hasFile('antd-with-locales.min.js.map')
-  .hasFile('antd.js')
-  .hasFile('antd.js.map')
-  .hasFile('antd.min.js')
-  .hasFile('antd.min.js.map')
-  .hasFile('reset.css');
+  .hasFile('antd-x-with-locales.js')
+  .hasFile('antd-x-with-locales.js.map')
+  .hasFile('antd-x-with-locales.min.js')
+  .hasFile('antd-x-with-locales.min.js.map')
+  .hasFile('antd-x.js')
+  .hasFile('antd-x.js.map')
+  .hasFile('antd-x.min.js')
+  .hasFile('antd-x.min.js.map');
 
 // eslint-disable-next-line no-console
 console.log(chalk.green('✨ `dist` directory is valid.'));
