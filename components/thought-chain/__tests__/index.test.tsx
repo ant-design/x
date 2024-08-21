ac87a52f55ca28b09ffa62dd3c43ae3dd5575731
🏎️ Yumo <yumo.imer@gmail.com>
2024-08-21 22:22:26 +0800

    test: update

--- components/thought-chain/__tests__/index.test.tsx
+++ components/thought-chain/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ThoughtChain from '../index';
-import { render } from '../../../tests/utils';
+import { render, fireEvent } from '../../../tests/utils';
 
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
@@ -21,14 +21,17 @@ const items: ThoughtChainItem[] = [
   {
     ...customizationProps,
     status: 'success',
+    key: 'test1',
   },
   {
     ...customizationProps,
     status: 'error',
+    key: 'test2',
   },
   {
     ...customizationProps,
     status: 'pending',
+    key: 'test3',
   },
 ];
 
@@ -46,9 +49,34 @@ describe('ThoughtChain Component', () => {
   });
 
   it('ThoughtChain component work', () => {
-    const { container } = render(<ThoughtChain items={items} />);
+    const { container } = render(<ThoughtChain items={items} collapsible />);
     const element = container.querySelector<HTMLUListElement>('.ant-thought-chain');
     expect(element).toBeTruthy();
     expect(element).toMatchSnapshot();
+  });
+
+  it('ThoughtChain component work with collapsible', () => {
+    const onExpand = jest.fn();
+    const App = () => {
+      const [expandedKeys, setExpandedKeys] = React.useState<string[]>([]);
+      return (
+        <ThoughtChain
+          items={items}
+          collapsible={{
+            expandedKeys,
+            onExpand: (keys) => {
+              onExpand(keys);
+              setExpandedKeys(keys);
+            },
+          }}
+        />
+      );
+    };
+    const { container } = render(<App />);
+    const element = container.querySelectorAll<HTMLDivElement>('.ant-thought-chain-item-header-box')[0];
+    fireEvent.click(element as Element);
+    expect(onExpand).toHaveBeenCalledWith(['test1']);
+    fireEvent.click(element as Element);
+    expect(onExpand).toHaveBeenCalledWith([]);
   });
 });
