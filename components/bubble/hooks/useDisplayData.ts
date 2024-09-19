d85473fe888170b67f6c96213c28047743dccaec
二货机器人 <smith3816@gmail.com>
2024-09-19 11:18:08 +0800

    chore: seq list

--- components/bubble/hooks/useDisplayData.ts
+++ components/bubble/hooks/useDisplayData.ts
@@ -1,48 +1,42 @@
+import { useEvent } from 'rc-util';
 import React from 'react';
 import { ListItemType } from './useListData';
-import { useEvent } from 'rc-util';
 
 export default function useDisplayData(items: ListItemType[]) {
-  const [typing, setTyping] = React.useState(false);
-  const [displayData, setDisplayData] = React.useState(items);
+  const [displayCount, setDisplayCount] = React.useState(items.length);
 
-  // const [displayCount, setDisplayCount] = React.useState(Math.max(1, items.length));
+  const displayList = React.useMemo(() => items.slice(0, displayCount), [items, displayCount]);
 
-  // const displayList = React.useMemo(() => items.slice(0, displayCount), [items, displayCount]);
+  const displayListLastKey = React.useMemo(() => {
+    const lastItem = displayList[displayList.length - 1];
+    return lastItem ? lastItem.key : null;
+  }, [displayList]);
 
   // When `items` changed, we replaced with latest one
   React.useEffect(() => {
-    // console.log('Compare Effect:', displayData, items);
-    // let notMatchIndex = displayData.length;
+    if (displayList.length && displayList.every((item, index) => item.key === items[index]?.key)) {
+      return;
+    }
 
-    // for (let i = 0; i < displayData.length; i += 1) {
-    //   if (displayData[i].key !== items[i]?.key) {
-    //     notMatchIndex = i;
-    //     console.log('fuck!', i);
-    //     break;
-    //   }
-    // }
-
-    // console.log('==>', notMatchIndex);
-    // setDisplayData(items.slice(0, notMatchIndex + 1));
-    if (
-      items.length !== displayData.length ||
-      items.some((item, index) => item.key !== displayData[index].key)
-    ) {
-      setTyping(true);
+    if (displayList.length === 0) {
+      setDisplayCount(1);
+    } else {
+      // Find diff index
+      for (let i = 0; i < displayList.length; i += 1) {
+        if (displayList[i].key !== items[i]?.key) {
+          setDisplayCount(i);
+          break;
+        }
+      }
     }
   }, [items]);
 
   // Continue to show if last one finished typing
   const onTypingComplete = useEvent((key: string | number) => {
-    const displayLen = displayData.length;
-    const lastKey = displayData[displayLen - 1]?.key;
-
-    if (typing && lastKey === key) {
-      setTyping(false);
-      setDisplayData(items.slice(0, displayLen + 1));
+    if (key === displayListLastKey) {
+      setDisplayCount(displayCount + 1);
     }
   });
 
-  return [displayData, onTypingComplete] as const;
+  return [displayList, onTypingComplete] as const;
 }
