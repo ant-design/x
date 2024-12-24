fd652ff1813cb7d9bbd050cbb8a54c3b4b4c7881
🏎️ Yumo <yumo.imer@gmail.com>
2024-12-24 10:32:19 +0800

    refactor: demo dom

--- components/sender/demo/focus.tsx
+++ components/sender/demo/focus.tsx
@@ -1,5 +1,5 @@
 import { Sender } from '@ant-design/x';
-import { Button, Space } from 'antd';
+import { Button, Flex } from 'antd';
 import React, { useRef } from 'react';
 
 import type { SenderRef } from '@ant-design/x';
@@ -13,47 +13,45 @@ const App: React.FC = () => {
   };
 
   return (
-    <Space direction="vertical" style={{ width: '100%' }}>
-      <Space wrap>
-        <Button
-          onClick={() => {
-            senderRef.current!.focus({
-              cursor: 'start',
-            });
-          }}
-        >
-          Focus at first
-        </Button>
-        <Button
-          onClick={() => {
-            senderRef.current!.focus({
-              cursor: 'end',
-            });
-          }}
-        >
-          Focus at last
-        </Button>
-        <Button
-          onClick={() => {
-            senderRef.current!.focus({
-              cursor: 'all',
-            });
-          }}
-        >
-          Focus to select all
-        </Button>
-        <Button
-          onClick={() => {
-            senderRef.current!.focus({
-              preventScroll: true,
-            });
-          }}
-        >
-          Focus prevent scroll
-        </Button>
-      </Space>
+    <Flex wrap gap={12}>
+      <Button
+        onClick={() => {
+          senderRef.current!.focus({
+            cursor: 'start',
+          });
+        }}
+      >
+        Focus at first
+      </Button>
+      <Button
+        onClick={() => {
+          senderRef.current!.focus({
+            cursor: 'end',
+          });
+        }}
+      >
+        Focus at last
+      </Button>
+      <Button
+        onClick={() => {
+          senderRef.current!.focus({
+            cursor: 'all',
+          });
+        }}
+      >
+        Focus to select all
+      </Button>
+      <Button
+        onClick={() => {
+          senderRef.current!.focus({
+            preventScroll: true,
+          });
+        }}
+      >
+        Focus prevent scroll
+      </Button>
       <Sender {...senderProps} />
-    </Space>
+    </Flex>
   );
 };
 
