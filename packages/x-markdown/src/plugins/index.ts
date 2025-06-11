4d29833a8d3d01c5ad47e6b38e9bcc7c6157c125
Mickey <951203214@qq.com>
2025-06-11 17:39:48 +0800

    chore: x-markdown

--- packages/x-markdown/src/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-import CodeHighLight from './CodeHighLight';
-import Latex from './Latex';
-// support any marked extensions
-import Footnote from 'marked-footnote';
-
-export { CodeHighLight, Latex, Footnote };
