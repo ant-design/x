a14eb310137cff553db475b19d18bf9611075dec
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-14 14:06:19 +0600

    docs: added reference in bubble as example

---
+++ components/bubble/demo/reference.tsx
@@ -0,0 +1,79 @@
+import { UserOutlined } from '@ant-design/icons';
+import { Bubble } from '@ant-design/x';
+import { Flex } from 'antd';
+import React from 'react';
+
+const fooAvatar: React.CSSProperties = {
+  color: '#f56a00',
+  backgroundColor: '#fde3cf',
+};
+
+const barAvatar: React.CSSProperties = {
+  color: '#fff',
+  backgroundColor: '#87d068',
+};
+
+const hideAvatar: React.CSSProperties = {
+  visibility: 'hidden',
+};
+
+const App: React.FC = () => (
+  <Flex gap="middle" vertical>
+    <Bubble
+      placement="start"
+      content="Good morning, how are you?"
+      avatar={{ icon: <UserOutlined />, style: fooAvatar }}
+      references={[
+        {
+          title: 'Ant Design',
+          url: 'https://ant.design',
+          description: 'Ant Design official website',
+        },
+        {
+          title: 'React',
+          url: 'https://react.dev',
+          description: 'React official documentation',
+        },
+        {
+          title: 'GitHub',
+          url: 'https://github.com',
+          description: 'GitHub homepage',
+        },
+        {
+          title: 'GitHub - Maifee Ul Asad',
+          url: 'https://github.com',
+        },
+      ]}
+    />
+    <Bubble
+      placement="start"
+      content="What a beautiful day!"
+      styles={{ avatar: hideAvatar }}
+      avatar={{}}
+      references={[
+        {
+          title: 'React',
+          url: 'https://react.dev',
+          description: 'React official documentation',
+        },
+        {
+          title: 'GitHub',
+          url: 'https://github.com',
+          description: 'GitHub homepage',
+        },
+        {
+          title: 'GitHub - Maifee Ul Asad',
+          url: 'https://github.com',
+        },
+      ]}
+    />
+    <Bubble
+      placement="end"
+      content="Hi, good morning, I'm fine!"
+      avatar={{ icon: <UserOutlined />, style: barAvatar }}
+    />
+    <Bubble placement="end" content="Thank you!" styles={{ avatar: hideAvatar }} avatar={{}} />
+  </Flex>
+);
+
+export default App;
