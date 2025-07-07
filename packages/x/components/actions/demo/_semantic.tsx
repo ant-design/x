627b561684d105682d99aa52b315f50def9d78a7
vanndxh <wangyuezhou.wyz@digital-engine.com>
2025-07-07 15:26:15 +0800

    fix: actions semantic opt

--- packages/x/components/actions/demo/_semantic.tsx
+++ packages/x/components/actions/demo/_semantic.tsx
@@ -56,6 +56,7 @@ const App: React.FC = () => {
         items={items}
         dropdownProps={{
           open: true,
+          getPopupContainer: (triggerNode) => triggerNode.parentElement!,
         }}
       />
     </SemanticPreview>
