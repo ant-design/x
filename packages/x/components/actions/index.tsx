86cc6be6a37ccd510ea20513f7fb9c4788ec5348
Mickey <951203214@qq.com>
2025-08-12 12:38:52 +0800

    test: husky

--- packages/x/components/actions/index.tsx
+++ packages/x/components/actions/index.tsx
@@ -109,7 +109,7 @@ const ForwardActions = React.forwardRef<ActionsRef, ActionsProps>((props, ref) =
 
 type CompoundedActions = typeof ForwardActions & {
   Feedback: typeof ActionsFeedback;
-};asddasdsadadaasdsadaasdasasdsadsaasdsadaasda
+};asddasdsadadaasdsadaasdasasdsadsaasdsadaasdaasda
 
 const Actions = ForwardActions as CompoundedActions;
 
