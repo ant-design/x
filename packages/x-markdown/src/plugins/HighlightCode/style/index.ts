4e46c9cea5717bbd584fbd42269b95b784acb347
Mickey <951203214@qq.com>
2025-09-03 14:51:03 +0800

    feat: 插件主题和国际化

--- packages/x-markdown/src/plugins/HighlightCode/style/index.ts
+++ packages/x-markdown/src/plugins/HighlightCode/style/index.ts
@@ -3,24 +3,6 @@ import type { GenerateStyle } from 'antd/es/theme/internal';
 import { genStyleHooks } from '../../theme/genStyleUtils';
 import type { FullToken, GetDefaultToken } from '../../theme/useToken';
 
-export type ComponentToken = {
-  /**
-   * @desc 标题背景颜色
-   * @descEN Title background color
-   */
-  colorBgTitle: string;
-  /**
-   * @desc 标题文本颜色
-   * @descEN Title text color
-   */
-  colorTextTitle: string;
-  /**
-   * @desc 代码块边框颜色
-   * @descEN Code block border color
-   */
-  colorBorderCode: string;
-};
-
 export interface HighlightCodeToken extends FullToken<'HighlightCode'> {}
 
 const genHighlightCodeStyle: GenerateStyle<HighlightCodeToken> = (token) => {
--- packages/x-markdown/src/plugins/Mermaid/style/index.ts
+++ packages/x-markdown/src/plugins/Mermaid/style/index.ts
@@ -3,32 +3,6 @@ import type { GenerateStyle } from 'antd/es/theme/internal';
 import { genStyleHooks } from '../../theme/genStyleUtils';
 import type { FullToken, GetDefaultToken } from '../../theme/useToken';
 
-export type ComponentToken = {
-  /**
-   * @desc 标题背景颜色
-   * @descEN Title background color
-   */
-  colorBgTitle: string;
-
-  /**
-   * @desc 标题文本颜色
-   * @descEN Title text color
-   */
-  colorTextTitle: string;
-
-  /**
-   * @desc 代码块边框颜色
-   * @descEN Code block border color
-   */
-  colorBorderCode: string;
-
-  /**
-   * @desc 图表边框颜色
-   * @descEN Graph border color
-   */
-  colorBorderGraph: string;
-};
-
 export interface MermaidToken extends FullToken<'Mermaid'> {}
 
 const genMermaidStyle: GenerateStyle<MermaidToken> = (token) => {
