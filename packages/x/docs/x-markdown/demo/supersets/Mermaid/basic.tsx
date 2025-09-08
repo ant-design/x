3d664bc8169697ecfcbb3348102b2fe1c6ca23e8
Mickey <951203214@qq.com>
2025-09-08 10:15:09 +0800

    fix: ts bubble

--- packages/x/docs/x-markdown/demo/supersets/Mermaid/basic.tsx
+++ packages/x/docs/x-markdown/demo/supersets/Mermaid/basic.tsx
@@ -1,7 +1,7 @@
 import { Bubble } from '@ant-design/x';
 import XMarkdown from '@ant-design/x-markdown';
 import Mermaid from '@ant-design/x-markdown/plugins/Mermaid';
-import { Flex, Button } from 'antd';
+import { Button, Flex } from 'antd';
 import React from 'react';
 
 const text = `
@@ -95,6 +95,11 @@ const App = () => {
 
       <Bubble
         content={text.slice(0, index)}
+        styles={{
+          content: {
+            width: 700,
+          },
+        }}
         contentRender={(content) => (
           <XMarkdown
             style={{ whiteSpace: 'normal' }}
