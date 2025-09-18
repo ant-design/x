580f1e5b55277d23de9159f5f5646947cd6f2d4e
Mickey <951203214@qq.com>
2025-09-18 12:07:19 +0800

    chore: 删除title

--- packages/x/.dumi/theme/layouts/DocLayout/index.tsx
+++ packages/x/.dumi/theme/layouts/DocLayout/index.tsx
@@ -21,11 +21,11 @@ import SidebarLayout from '../SidebarLayout';
 
 const locales = {
   cn: {
-    title: 'Ant Design X',
+    title: 'Ant Design X - AI界面解决方案',
     description: '轻松打造 AI 驱动的界面。',
   },
   en: {
-    title: 'Ant Design X',
+    title: 'Ant Design X - AI interface solution',
     description: 'Craft AI-driven interfaces effortlessly.',
   },
 };
