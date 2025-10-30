20870ae728de8433691edb34996de05655af0e4d
Mickey <951203214@qq.com>
2025-10-30 23:15:19 +0800

    feat: 更新文档

--- packages/x/components/actions/demo/fadeIn.tsx
+++ packages/x/components/actions/demo/fadeIn.tsx
@@ -2,19 +2,8 @@ import { RedoOutlined } from '@ant-design/icons';
 import { Actions, ActionsProps } from '@ant-design/x';
 import { Button, Flex, message, Pagination } from 'antd';
 import React, { useState } from 'react';
-import { useIntl } from 'react-intl';
-
-const LOCALE_MARKDOWN = {
-  'en-US': {
-    reRender: 'Re-Render',
-  },
-  'zh-CN': {
-    reRender: '重新渲染',
-  },
-};
 
 const App: React.FC = () => {
-  const { locale } = useIntl();
   // pagination
   const [curPage, setCurPage] = useState(1);
   const [key, setKey] = useState(0);
@@ -52,7 +41,7 @@ const App: React.FC = () => {
   return (
     <Flex gap="middle" vertical>
       <Button style={{ alignSelf: 'flex-end' }} onClick={() => setKey(key + 1)}>
-        {LOCALE_MARKDOWN[locale as keyof typeof LOCALE_MARKDOWN].reRender}
+        Re-Render
       </Button>
 
       <Actions key={key} fadeIn items={actionItems} onClick={onClick} variant="borderless" />
