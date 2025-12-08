51fa343cca6ee8d159c6b552b04ee2e353893a9f
Div627 <chenyang_nn@163.com>
2025-12-08 16:40:34 +0800

    fix: fix cr

--- packages/x/components/mermaid/index.tsx
+++ packages/x/components/mermaid/index.tsx
@@ -1,14 +1,5 @@
-import React from 'react';
-import type { MermaidProps, MermaidType } from './Mermaid';
+import Mermaid from './Mermaid';
 
-const LazyMermaid = React.lazy(() => import('./Mermaid'));
+export type { MermaidProps, MermaidType } from './Mermaid';
 
-const MermaidWrapper: React.FC<MermaidProps> = (props) => (
-  <React.Suspense fallback={<div>Loading Mermaid</div>}>
-    <LazyMermaid {...props} />
-  </React.Suspense>
-);
-
-export type { MermaidProps, MermaidType };
-
-export default MermaidWrapper;
+export default Mermaid;
