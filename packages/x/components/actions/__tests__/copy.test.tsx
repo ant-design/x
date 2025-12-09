536d01497ad5841732881e224d299b92d722fc85
Mickey <951203214@qq.com>
2025-12-09 18:05:32 +0800

    test: actions

--- packages/x/components/actions/__tests__/copy.test.tsx
+++ packages/x/components/actions/__tests__/copy.test.tsx
@@ -1,4 +1,3 @@
-import { XProvider } from '@ant-design/x';
 import React from 'react';
 import { fireEvent, render } from '../../../tests/utils';
 import ActionsCopy from '../ActionsCopy';
@@ -6,55 +5,35 @@ import ActionsCopy from '../ActionsCopy';
 describe('ActionsCopy', () => {
   it('renders with text', () => {
     const { container } = render(
-      <XProvider>
-        <ActionsCopy text="hello" />
-      </XProvider>,
+      <ActionsCopy text="hello" />,
     );
     expect(container.querySelector('.ant-actions-copy')).toBeTruthy();
   });
   it('renders with no text', () => {
-    const { container } = render(
-      <XProvider>
-        <ActionsCopy />
-      </XProvider>,
-    );
+    const { container } = render(<ActionsCopy />);
     expect(container.querySelector('.ant-actions-copy')).toBeTruthy();
   });
   it('renders with icon', () => {
-    const { container } = render(
-      <XProvider>
-        <ActionsCopy text="copy" icon={<span data-testid="icon" />} />
-      </XProvider>,
-    );
+    const { container } = render(<ActionsCopy text="copy" icon={<span data-testid="icon" />} />);
     expect(container.querySelector('[data-testid="icon"]')).toBeTruthy();
   });
 
   it('supports custom className and prefixCls', () => {
     const { container } = render(
-      <XProvider>
-        <ActionsCopy text="test" className="my-class" prefixCls="my-prefix" />
-      </XProvider>,
+      <ActionsCopy text="test" className="my-class" prefixCls="my-prefix" />,
     );
     expect(container.querySelector('.my-class')).toBeTruthy();
     expect(container.querySelector('.my-prefix-copy')).toBeTruthy();
   });
 
   it('supports rootClassName', () => {
-    const { container } = render(
-      <XProvider>
-        <ActionsCopy text="test" rootClassName="root-class" />
-      </XProvider>,
-    );
+    const { container } = render(<ActionsCopy text="test" rootClassName="root-class" />);
     expect(container.querySelector('.root-class')).toBeTruthy();
   });
 
   it('triggers copy event', () => {
     // 由于 antd CopyBtn 内部实现，直接模拟点击
-    const { container } = render(
-      <XProvider>
-        <ActionsCopy text="copied!" />
-      </XProvider>,
-    );
+    const { container } = render(<ActionsCopy text="copied!" />);
     const btn = container.querySelector('span');
     expect(btn).toBeTruthy();
     if (btn) {
