ad1c02ca524cfa4e5626b31b1b524e575b8225a7
Mickey <951203214@qq.com>
2026-02-25 16:48:56 +0800

    feat: prompt

--- packages/x/.dumi/theme/builtins/PromptTemplate.tsx
+++ packages/x/.dumi/theme/builtins/PromptTemplate.tsx
@@ -1,43 +1,13 @@
 import { XMarkdown } from '@ant-design/x-markdown';
 import { Card, Typography } from 'antd';
-import { createStyles, css } from 'antd-style';
 import React from 'react';
-import useLocale from '../../hooks/useLocale';
 
 const { Text } = Typography;
-
-const useStyle = createStyles(({ token }) => ({
-  container: css`
-    border-radius: ${token.borderRadiusLG}px;
-    border: 1px solid ${token.colorSplit};
-  `,
-  item: css`
-    a {
-      width: 100%;
-    }
-    cursor: pointer;
-    transition: all ${token.motionDurationMid} ${token.motionEaseInOut};
-    &:hover {
-      background-color: ${token.colorFillQuaternary};
-    }
-  `,
-  itemMeta: css`
-    padding-inline: ${token.padding}px};
-  `,
-}));
 
 const PromptTemplate: React.FC<{
   title: string;
   children: React.ReactNode;
 }> = (props) => {
-  console.log(props, 'props');
-  // ======================== locale =========================
-  const [_, lang] = useLocale();
-
-  // ======================== style =========================
-
-  const { styles } = useStyle();
-
   // ======================== Render ========================
   return (
     <Card
