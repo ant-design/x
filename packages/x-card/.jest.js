741b16340f136b30dc456272206f3a7214173ebe
Mickey <951203214@qq.com>
2026-03-04 10:55:57 +0800

    feat: x-card

---
+++ packages/x-card/.jest.js
@@ -0,0 +1,20 @@
+const { createJestConfig } = require('@ant-design/tools/lib/jest');
+
+module.exports = createJestConfig({
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{ts,tsx}',
+    '<rootDir>/src/**/*.{test,spec}.{ts,tsx}',
+    '<rootDir>/tests/**/*.{test,spec}.{ts,tsx}',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/demo/**',
+    '!src/**/__tests__/**',
+  ],
+});
