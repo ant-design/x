9ff58c0fe52c2cbc4934388901b36f194acae8b0
张帆 <1029129827@qq.com>
2026-09-17 17:42:26 +0800

    feat(actions): support custom tooltip

--- packages/x/components/actions/__tests__/action-item.test.tsx
+++ packages/x/components/actions/__tests__/action-item.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '../../../tests/utils';
+import { fireEvent, render } from '../../../tests/utils';
 import ActionsItem from '../ActionsItem';
 
 describe('Actions.Item', () => {
@@ -7,5 +7,42 @@ describe('Actions.Item', () => {
     const { getByText } = render(<ActionsItem defaultIcon="default-icon" />);
     expect(getByText('default-icon')).toBeTruthy();
     render(<ActionsItem defaultIcon="default-icon" status={'xxx' as any} />);
+  });
+
+  it('does not render tooltip when tooltip is false', () => {
+    const { container } = render(
+      <ActionsItem defaultIcon="default-icon" label="Default label" tooltip={false} />,
+    );
+    expect(container.querySelector('.ant-tooltip')).toBeNull();
+  });
+
+  it('renders tooltip with default label', async () => {
+    const { container, findByText } = render(
+      <ActionsItem defaultIcon="default-icon" label="Default label" />,
+    );
+
+    fireEvent.mouseEnter(container.querySelector('.ant-actions-item')!);
+
+    expect(await findByText('Default label')).toBeInTheDocument();
+  });
+
+  it('renders tooltip with custom text', async () => {
+    const { container, findByText } = render(
+      <ActionsItem defaultIcon="default-icon" label="Default label" tooltip="Custom tooltip" />,
+    );
+    fireEvent.mouseEnter(container.querySelector('.ant-actions-item')!);
+    expect(await findByText('Custom tooltip')).toBeInTheDocument();
+  });
+
+  it('renders tooltip with custom props', async () => {
+    const { container, findByText } = render(
+      <ActionsItem
+        defaultIcon="default-icon"
+        label="Default label"
+        tooltip={{ title: 'Custom tooltip' }}
+      />,
+    );
+    fireEvent.mouseEnter(container.querySelector('.ant-actions-item')!);
+    expect(await findByText('Custom tooltip')).toBeInTheDocument();
   });
 });
